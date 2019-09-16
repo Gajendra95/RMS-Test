d5c113d62bf274e1e1fa5adc0b1bba20c91194c8
Gajendra95 <44360095+Gajendra95@users.noreply.github.com>
2019-09-16 09:48:44 +0530

    Update Class1.cs

--- myrmstest/Class1.cs
+++ myrmstest/Class1.cs
@@ -14,7 +14,7 @@ namespace myrmstest
         [Test]
         public void test1()
         {
-            string Url = "http://172.16.51.47/RMSPublish/Login.aspx";
+            string Url = "http://172.16.51.51/RMSPublish/Login.aspx";
             IWebDriver driver = new ChromeDriver();
 
             //IWebDriver driver = new ChromeDriver();
